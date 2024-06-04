28b6797543537155e68efdcffb1fb2d82bb89178
Tran Duc Manh <tranducmanh235@gmail.com>
2024-06-04 14:31:03 +0700

    React Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+// import { useDispatch, useSelector } from "react-redux";
+// import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+import MyComponent from "./components/MyComponent";
+
 const App = () => {
-    const count = useSelector((state) => state.counter.count);
-    const dispatch = useDispatch();
+    // const count = useSelector((state) => state.counter.count);
+    // const dispatch = useDispatch();
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello World Tsuyoix4</p>
-                <div>Count = {count}</div>
-                <button onClick={() => dispatch(increaseCounter())}>
-                    Increase
-                </button>
-                <button onClick={() => dispatch(decreaseCounter())}>
-                    Decrease
-                </button>
-            </header>
+        <div>
+            Hello world! &amp; Welcome!!!
+            <MyComponent />
         </div>
+        // <div className="App">
+        //     <header className="App-header">
+        //         <img src={logo} className="App-logo" alt="logo" />
+        //         <p>Hello World Tsuyoix4</p>
+        //         <div>Count = {count}</div>
+        //         <button onClick={() => dispatch(increaseCounter())}>
+        //             Increase
+        //         </button>
+        //         <button onClick={() => dispatch(decreaseCounter())}>
+        //             Decrease
+        //         </button>
+        //     </header>
+        // </div>
     );
 };
 
---
+++ src/components/MyComponent.js
@@ -0,0 +1,20 @@
+/*
+    class component
+    function component
+*/
+
+import React from "react";
+
+class MyComponent extends React.Component {
+    // JSX
+    render() {
+        return (
+            <div>
+                my first component abcxyz
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
