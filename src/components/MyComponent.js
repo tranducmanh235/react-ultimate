fa48393167467eb11e121b7b18b785554134cea1
Tran Duc Manh <tranducmanh235@gmail.com>
2024-06-04 15:46:24 +0700

    react dom

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -12,12 +12,23 @@ class MyComponent extends React.Component {
         age: 25,
     };
 
+    handleClick(event) {
+        console.log(">>> Click me!");
+        console.log("My Name is ", this.state.name);
+    }
+
+    handleOnMoverover(event) {
+        console.log(event.pageX);
+    }
+
     // JSX
     render() {
         return (
             <div>
                 My Name is {this.state.name + " "}
                 and My address is {this.state.address}
+                <button onMouseOver={this.handleOnMoverover}>Hover me</button>
+                <button onClick={this.handleClick}>Click me</button>
             </div>
         );
     }
